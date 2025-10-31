32fdfcd58e462e4d639de8f253b1063fbff5c4b1
Isis Abraham <isiscavalcantiabraham@gmail.com>
2025-10-31 08:28:01 -0400

    Create daily and monthly KPI views in SQL

--- contoso-retaildw/01_sales_analytics/sql/01_create_views_kpis.sql
+++ contoso-retaildw/01_sales_analytics/sql/01_create_views_kpis.sql
@@ -1,1 +1,40 @@
+USE ContosoRetailDW;
+GO
+
+
+IF OBJECT_ID('dbo.vw_DailyKPI') IS NOT NULL
+  DROP VIEW dbo.vw_DailyKPI;
+GO
+CREATE VIEW dbo.vw_DailyKPI AS
+SELECT
+    d.FullDateAlternateKey AS [Date],
+    SUM(fs.SalesQuantity) AS Qty,
+    SUM(fs.SalesAmount)   AS Revenue,
+    CASE 
+      WHEN SUM(fs.SalesQuantity) = 0 THEN 0
+      ELSE SUM(fs.SalesAmount) * 1.0 / SUM(fs.SalesQuantity)
+    END AS AvgOrderValue
+FROM FactOnlineSales fs
+JOIN DimDate d ON d.DateKey = fs.DateKey
+GROUP BY d.FullDateAlternateKey;
+GO
+
+IF OBJECT_ID('dbo.vw_MonthlyKPI') IS NOT NULL
+  DROP VIEW dbo.vw_MonthlyKPI;
+GO
+CREATE VIEW dbo.vw_MonthlyKPI AS
+SELECT
+    d.CalendarYear,
+    d.CalendarMonth,
+    EOMONTH(d.FullDateAlternateKey) AS MonthEnd,
+    SUM(fs.SalesQuantity) AS Qty,
+    SUM(fs.SalesAmount)   AS Revenue,
+    CASE 
+      WHEN SUM(fs.SalesQuantity) = 0 THEN 0
+      ELSE SUM(fs.SalesAmount) * 1.0 / SUM(fs.SalesQuantity)
+    END AS AvgOrderValue
+FROM FactOnlineSales fs
+JOIN DimDate d ON d.DateKey = fs.DateKey
+GROUP BY d.CalendarYear, d.CalendarMonth, EOMONTH(d.FullDateAlternateKey);
+GO
 
